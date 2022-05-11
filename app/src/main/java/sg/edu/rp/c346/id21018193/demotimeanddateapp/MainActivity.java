0ec49c9d433a1fba97971d4968726851e2e89dfc
lee zhuo hong <21018193@myrp.edu.sg>
2022-05-11 10:12:55 +0800

    .

--- app/src/main/java/sg/edu/rp/c346/id21018193/demotimeanddateapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/demotimeanddateapp/MainActivity.java
@@ -3,12 +3,27 @@ package sg.edu.rp.c346.id21018193.demotimeanddateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatePicker dp;
+    TimePicker tp;
+    Button btnDisplayDate;
+    Button btnDisplayTime;
+    TextView tvDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        btnDisplayDate = findViewById(R.id.buttonDisplayDate);
+        btnDisplayTime = findViewById(R.id.buttonDisplayTime);
     }
 }
